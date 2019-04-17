7f52edeb5f9a24b72a53e9d2f8449b569e900a71
Yakov Butenko <wx1324@yandex.ru>
2019-04-17 16:13:57 +0300

    task3 end

--- src/ru/butenko/task3/MultiplicationTable.java
+++ src/ru/butenko/task3/MultiplicationTable.java
@@ -6,7 +6,7 @@ public class MultiplicationTable {
 
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
-                System.out.print(i*j + " ");
+                System.out.print(i * j + " ");
             }
             System.out.println("");
         }
--- src/ru/butenko/task3/Progression.java
+++ src/ru/butenko/task3/Progression.java
@@ -1,4 +1,60 @@
 package ru.butenko.task3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Progression {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите тип прогрессии (арифметическая или геометрическая)");
+
+        boolean isWorking = true;
+
+        while (isWorking) {
+            String progressionType = reader.readLine().toLowerCase();
+            switch (progressionType) {
+                case ("арифметическая"):
+                    System.out.println("Введите разность арифметической прогрессии");
+                    int difProgression = Integer.parseInt(reader.readLine());
+
+                    System.out.println("Введите количество членов прогрессии");
+                    int countProgression = Integer.parseInt(reader.readLine());
+
+                    System.out.println("Введите начальное число прогрессии");
+                    int startNumber = Integer.parseInt(reader.readLine());
+
+                    for (int i = 0; i < countProgression; i++) {
+                        System.out.println(startNumber);
+                        startNumber += difProgression;
+                    }
+                    isWorking = false;
+                    break;
+
+                case ("геометрическая"):
+                    System.out.println("Введите знаменатель прогрессиии");
+                    int progressionDenominator = Integer.parseInt(reader.readLine());
+
+                    System.out.println("Введите количество членов прогрессии");
+                    int countGeometricalProgression = Integer.parseInt(reader.readLine());
+
+                    System.out.println("Введите начальное число прогрессии");
+                    int startGeometricalNumber = Integer.parseInt(reader.readLine());
+
+                    for (int i = 0; i < countGeometricalProgression; i++) {
+                        System.out.println(startGeometricalNumber);
+                        startGeometricalNumber *= progressionDenominator;
+                    }
+                    isWorking = false;
+                    break;
+
+                default:
+                    System.out.println("Вы неправильно ввели название прогрессии. Попробуйте ещё раз.");
+
+            }
+        }
+    }
 }
