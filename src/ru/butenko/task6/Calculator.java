94161ea831ac2dc01da1d0587f5e3008c53fc617
Yakov Butenko <wx1324@yandex.ru>
2019-04-26 14:31:00 +0300

    обнова3

--- src/ru/butenko/task6/Calculator.java
+++ src/ru/butenko/task6/Calculator.java
@@ -2,6 +2,12 @@ package ru.butenko.task6;
 
 public class Calculator {
 
+    public final double PI = 3.14;
+    /* согласно первому пункту ДЗ, решил просто добавить модификатор final
+       для примитива,  данном случае числу Пи, так как это константа
+     */
+
+
     public static double sum (double a, double b){
         return a + b;
     }
