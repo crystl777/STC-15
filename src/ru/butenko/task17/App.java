7fd855ad469fcad46d513abc294549fb83fc54e1
Yakov Butenko <wx1324@yandex.ru>
2019-06-05 09:01:06 +0300

    comporator

---
+++ src/ru/butenko/task17/App.java
@@ -0,0 +1,24 @@
+package ru.butenko.task17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        personList.add(new Person("Василий", 26));
+        personList.add(new Person("Николай", 47));
+        personList.add(new Person("Артём", 21));
+        personList.add(new Person("Ксения", 54));
+        personList.add(new Person("Владимир", 63));
+
+        System.out.println(personList);
+
+        Collections.sort(personList, new PersonSuperComparator());
+
+        System.out.println(personList);
+
+    }
+}
---
+++ src/ru/butenko/task17/PersonSuperComparator.java
@@ -0,0 +1,19 @@
+package ru.butenko.task17;
+
+import java.util.Comparator;
+
+public class PersonSuperComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        int value = o1.getName().compareTo(o2.getName());
+
+        if (value == 0) {
+            if (o1.getAge() == o2.getAge()) {
+                return value;
+            } else if (o1.getAge() > o2.getAge()) {
+                return o1.getAge() - o2.getAge();
+            } else return o2.getAge() - o1.getAge();
+        }
+        return value;
+    }
+}
